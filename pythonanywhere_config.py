49191d89889114a01539b0890fffa4bb5abfe4f6
daniiyalw <daniiyalw@gmail.com>
2026-01-16 17:14:06 +0500

    Create pythonanywhere_config.py

---
+++ pythonanywhere_config.py
@@ -0,0 +1,11 @@
+# PythonAnywhere specific config
+import sys
+import os
+
+# Add your app directory to path
+path = os.path.dirname(os.path.abspath(__file__))
+if path not in sys.path:
+    sys.path.append(path)
+
+# Import your app
+from app import app as application
